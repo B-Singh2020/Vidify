ca414b7e22a94175397c1b87fbbe9b91a7df0e08
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-07-09 15:00:24 -0400

    Add project files.

---
+++ Vidify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidify.Startup))]
+namespace Vidify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
