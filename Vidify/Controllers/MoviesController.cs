8af94af7f75250fb00e9dea6e4126695a79a63dd
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-07-09 21:11:09 -0400

    Add random movie method in controller and lumen bootstrap

---
+++ Vidify/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidify.Models;
+
+namespace Vidify.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random , this action will get random movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Puss in Boots"};  
+            return View(movie);                            //return corresponding view created for method
+        }
+    }
+}
