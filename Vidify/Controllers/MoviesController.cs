a54837490f8258bf939b292588641191504000e1
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-07-22 13:55:12 -0400

    Added in forms to create and edit data for movies

--- Vidify/Controllers/MoviesController.cs
+++ Vidify/Controllers/MoviesController.cs
@@ -30,6 +30,34 @@ namespace Vidify.Controllers
             return View(movies);
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres,
+
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)  //method called to get existing movie from db to edit
+        {
+            var movie = _context.Movies.SingleOrDefault(c  => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie, 
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }  
+
         public ActionResult Details(int id)  //returns view of movies or not found
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
@@ -88,7 +116,27 @@ namespace Vidify.Controllers
                                             }
                                             */
 
+        [HttpPost]
+        public ActionResult Save(Movie movie)        //method to save changes or add movie to db 
+        {
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+         else
+            {
+                var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
 
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
     }
 
 }
--- Vidify/Models/Movie.cs
+++ Vidify/Models/Movie.cs
@@ -9,17 +9,22 @@ namespace Vidify.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
         public Genre Genre { get; set; }
-        public byte GenreId { get; set; }
 
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
