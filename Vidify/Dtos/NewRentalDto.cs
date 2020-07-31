ccbf7f0ec21686b75e8277ee4b3edc2ef90c99b5
Bhupinder Singh <48160814+B-Singh2020@users.noreply.github.com>
2020-07-31 19:25:39 -0400

    added simple rental api

---
+++ Vidify/Dtos/NewRentalDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+
+
+namespace Vidify.Dtos
+{
+    
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
